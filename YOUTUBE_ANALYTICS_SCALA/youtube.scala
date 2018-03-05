b13b56a98be16899ae8085aa9295699aa801bd33
aneeshpartha <aneeshpartha5@gmail.com>
2018-03-04 22:55:51 -0600

    updated program

--- YOUTUBE_ANALYTICS_SCALA/youtube.scala
+++ YOUTUBE_ANALYTICS_SCALA/youtube.scala
@@ -6,52 +6,63 @@ import scala.io.Source
 import java.io.PrintWriter
 import java.io.File
 import java.io._
+import scala.collection.immutable
 
-object  youtubeanalytics extends Application {
+object  youtubeanalytics extends App {
 
-
+  try {
     def filter(data: Iterator[String]) = {
-    val filter1 = data.drop(1)
 
-    val filter2 = filter1.filter(rec => rec.startsWith("\\n") == false)
+      val filter1 = data.drop(1)
 
-    val filter3 = filter2.filter(rec => rec.startsWith("http") == false)
+      val filter2: Iterator[String] = filter1.filter(rec => rec.startsWith("\\n") == false)
 
-    val filter4 = filter3.filter(rec => rec.contains(",") == true)
+      val filter3 = filter2.filter(rec => rec.startsWith("http") == false)
 
-    val filter5 = filter4.filter(rec => rec.startsWith(" ") == false)
+      val filter4 = filter3.filter(rec => rec.contains(",") == true)
 
-    val filter6 = filter5.filter( rec => rec.split(",")(0).length == 11)
+      val filter6 = filter4.filter(rec => rec.split(",")(0).length == 11)
 
-    val filter7 = filter6.filter( rec => (rec.count(_ == ',') == 15))
-    
-    filter7
+      val filter7: Iterator[String] = filter6.filter(rec => (rec.count(_ == ',') == 15))
+
+      filter7
+    }
+
+
+    def process(data: Iterator[String]) = {
+
+
+      val b = data.map(rec => {
+
+        val d = rec.split(",")
+        (d(1), d(7).toInt, d(0), d(2), d(3), d(8), d(9))
+      })
+
+
+      val c = b.toSeq.sortBy((rec => (rec._1, rec._2))).reverse
+
+
+      val d: Map[String, Seq[(String, Int, String, String, String, String, String)]] = c.groupBy(rec => (rec._1))
+
+      d
+
+
+    }
+
+    val srcfile = Source.fromFile(args(0))("UTF-8").getLines()
+    val filtereddata = filter(srcfile)
+    val processeddata = process(filtereddata)
+
+    println("Date" + "\t" + "No of views" + "\t" + "Video_id" + "\t" + "channel" + "\t\t\t" + "Description" + "\t" + "likes")
+
+    val data = processeddata.map(rec => rec._2(0)).map(rec => (rec._1 + "\t" + rec._2 + "\t" + rec._3 +
+      "\t" + rec._4 + "\t\t\t" + rec._5 + "\t" + rec._6 + "\t" + rec._7))
+
+    data.foreach(println)
   }
-
-
-
-  def process(data: Iterator[String]) = {
-
-       val process1 = data.map(rec => {
-
-       val d = rec.split(",")
-      (d(1), d(7).toInt, d(0), d(2), d(3), d(8), d(9))
-    })
-
-
-      val process2 = process1.toSeq.sortBy((rec => (rec._1,rec._2))).reverse
-
-
-      val process3 = process2.groupBy( rec => (rec._1))
-
-      process3
-
+  catch
+  {
+    case num:NumberFormatException => print("There is a number format exception")
   }
-
-  val srcfile = Source.fromFile("G:\\Datasets\\youtube\\FRvideos\\FRvideos.csv")("UTF-8").getLines()
-  val filtereddata = filter(srcfile)
-  val processeddata = process(filtereddata)
-
 }
-
 
